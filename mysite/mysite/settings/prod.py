604b1988e576434d90de0e4d2f99e76fa6bb8ef0
gustos <jucie15@nate.com>
2019-08-22 01:00:09 +0900

    디비 설정 정보 환경변수에서 가져오기

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -7,3 +7,14 @@ ALLOWED_HOSTS = ['15.164.126.227', ]
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PW'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '5432',
+    },
+}
